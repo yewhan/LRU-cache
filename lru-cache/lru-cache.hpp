48f6be313895605a7ed91e5ee75a95232d5aefcd
yewhan <euanhughesj@gmail.com>
2024-04-11 12:53:19 +0100

    defined basic structure for LRU cache

--- lru-cache/lru-cache.hpp
+++ lru-cache/lru-cache.hpp
@@ -1,6 +1,32 @@
 #ifndef LRU_CACHE_HPP
 #define LRU_CACHE_HPP
 
+#include <iostream>
+#include <unordered_map>
+
+class LRUCache {
+    struct node {
+        int key;
+        int data;
+        struct node *prev;
+        struct node *next;
+
+        node(int k, int d) : key(k), data(d), prev(nullptr), next(nullptr) {}
+    };
+
+    std::unordered_map<int, struct node> cache;
+    node head;
+    node tail;
+    int size;
+
+    public:
+
+
+    private:
+
+
+};
+
 
 
 #endif
