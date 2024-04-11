578a766bce74d74c87e7746f31a93d429b78edee
yewhan <euanhughesj@gmail.com>
2024-04-11 14:44:30 +0100

    actually implemented LRUCache destructor

--- lru-cache/lru-cache.cpp
+++ lru-cache/lru-cache.cpp
@@ -9,3 +9,12 @@ LRUCache::LRUCache(int size) : size(size) {
     head->next = tail;
     tail->prev = head;
 }
+
+LRUCache::~LRUCache() {
+    // delete all nodes
+    while (head != nullptr) { // tail->next = nullptr
+        Node* tmp = head;
+        head = head->next;
+        delete tmp;
+    }
+}
